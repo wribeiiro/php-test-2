490c0cb04ad11254eda9f1b39b31b703a27a44c6
Wellisson Ribeiro <welleh10@gmail.com>
2020-12-23 22:18:10 -0300

    working in tests

--- app/Models/ProductModel.php
+++ app/Models/ProductModel.php
@@ -6,14 +6,36 @@ use App\System\Database;
 
 class ProductModel extends Database {
 
-    public function __construct()
-    {
-        parent::__construct();
-        $this->table = "products";
-    }
-
     protected string $description;
     protected int $product_type_id;
     protected float $price;
 
+    public function __construct() {
+        parent::__construct();
+        $this->table = "products";
+    }
+
+    public function setDescription(string $description): void {
+        $this->description = $description;
+    }
+
+    public function setProductType(int $productTypeId): void {
+        $this->product_type_id = $productTypeId;
+    }
+
+    public function setPrice(float $price): void {
+        $this->price = $price;
+    }
+
+    public function getDescription(): string {
+        return $this->description;
+    } 
+
+    public function getProductTypeId(): int {
+        return $this->product_type_id;
+    }
+
+    public function getPrice(): float {
+        return $this->price;
+    }
 }
--- tests/ProductTest.php
+++ tests/ProductTest.php
@@ -3,22 +3,32 @@
 namespace Test;
 
 use PHPUnit\Framework\TestCase;
+use App\Models\ProductModel;
 
 class ProductTest extends TestCase {
 
-    public function testPriceIsFloat() {
-        $this->assertIsFloat(5.75);
+    /**
+     * @var ProductModel
+     */
+    private $product;
+
+    protected function setUp(): void {
+        $this->product = new ProductModel();
+
+        $this->product->setDescription('Product Test');
+        $this->product->setProductType(0);
+        $this->product->setPrice(5.25);
     }
 
-    public function testPriceIsNotFloat() {
-        $this->assertIsNotFloat(1);
+    public function testFullDescription() {
+        $this->expectException(FullDescriptionException::class);
+    }
+
+    public function testPriceIsFloat() {
+        $this->assertIsFloat($this->product->getPrice());
     }
 
     public function testPriceIsGreatThanZero() {
-        $this->assertGreaterThan(0, 1);
-    }
-
-    public function testPriceIsNotGreatThanZero() {
-        $this->assertGreaterThan(0, 0);
+        $this->assertGreaterThan(0, $this->product->getPrice());
     }
 }
